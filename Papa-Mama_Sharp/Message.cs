eeb239504674593b35c8af55e3b262a675cbebbd
AKurock <78851466+AKurock@users.noreply.github.com>
2021-04-07 12:05:57 +0300

    PapaMamaCSharp

---
+++ Papa-Mama_Sharp/Message.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Papa_Mama
+{
+    class Message
+    {
+        public static int n_rep;
+        private static int delay;
+        private static int repeat;
+        private static string value;
+        public static object l = new object();
+        public Message(string _value, int _repeat, int _delay)
+        {  
+            value = _value;
+            repeat = _repeat; 
+            delay = _delay; 
+        }
+
+        public  void ToSay() 
+        {
+            int i = 0;
+            while( i < repeat && n_rep > 0)
+            {
+                lock (l)
+                {
+                    Console.WriteLine(value);
+                    n_rep--;
+                    i++;
+                    Thread.Sleep(delay);
+                }
+
+            }
+           
+        }
+    }
+}
